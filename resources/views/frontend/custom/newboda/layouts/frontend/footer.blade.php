04c18ad7b8b7eeb14fb1b92652a07a008a9dad8e
slimerence <39113678+slimerence@users.noreply.github.com>
2018-08-07 13:39:51 +1000

    footer新版

--- resources/views/frontend/custom/newboda/layouts/frontend/footer.blade.php
+++ resources/views/frontend/custom/newboda/layouts/frontend/footer.blade.php
@@ -1,107 +1,91 @@
-<style>
-    .footer h3{
-        font-size: 20px;
-        margin: 0 0 10px 0;
-        color: #fff;
-        font-weight: bold;
-    }
-</style>
-<div class="footer">
-    <div class="redbgtop">
-        <div class="wrap clearfix">
-            <div class="block f-nav">
-                <h3>NAVIGATION:</h3>
-                <ul class="f-ul-nav">
-                    <li>
-                        <a href="/" class="current">HOME</a>
-                    </li>
-                    <li>
-                        <a href="/aboutus">ABOUT US</a>
-                    </li>
-                    <li>
-                        <a href="/services">SERVICES</a>
-                    </li>
-                    <li>
-                        <a href="/gallery">GALLERY</a>
-                    </li>
-                    <li>
-                        <a href="/hiring">HIRING</a>
-                    </li>
-                    <li>
-                        <a href="/getrates">GET RATES</a>
-                    </li>
-                    <li>
-                        <a href="/contactus">CONTACT US</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="block f-share">
-                <h3>SOCIAL MEDIA</h3>
-                <div class="shareiconbox clearfix">
-                    <a class="jiathis_button_fb" title='facebook'>
-                        <i class="fa fa-facebook"></i>
-                    </a>
-                    <a class="jiathis_button_twitter" title='twitter'>
-                        <i class="fa fa-twitter"></i>
-                    </a>
-                    <a href="http://instagram.com/" title='instagram'>
-                        <i class="fa fa-instagram"></i>
-                    </a>
-                </div>
-                <div class="erweimabox">
-                    <img src="{{asset('images/newfrontend/newerweima.jpg')}}" pagespeed_url_hash="1457276618" onload="">
+
+<section class="color-bg text-center reblock" id="footer-color">
+    <i class="fa fa-facebook-official" aria-hidden="true"></i>
+    <i class="fa fa-instagram" aria-hidden="true"></i>
+    <i class="fa fa-twitter" aria-hidden="true"></i>
+    <i class="fa fa-weixin" aria-hidden="true"></i>
+</section>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="footer-widget footer-contact">
+                    <h3>CONTACT US</h3>
+                    <ul class="no-list-style">
+                        <li><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i> {{ $siteConfig->contact_address }}</li>
+                        <li><i class="fa fa-phone fa-fw" aria-hidden="true"></i> {{ $siteConfig->contact_phone }} </li>
+                        <li><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i> {{ $siteConfig->contact_email }} </li>
+                    </ul>
                 </div>
             </div>
-            <div class="block f-info">
-                <h3>CONTACT US:</h3>
-                <ul class="f-ul-info">
-                    <li>
-                        <div itemscope itemtype="http://schema.org/LocalBusiness">
-                            <a itemprop="url" href="{{url('/')}}"><div itemprop="name"><strong>New Boda Carpet Cleaning and House Cleaning Expert</strong></div>
-                            </a>
-                            <span itemprop="telephone"><a href="tel:(03) 9563 2204">(03) 9563 2204</a></span><br>
+            <div class="col-lg-6">
+                <div class="footer-widget">
+                    <h3>OUR SERVICES</h3>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <ul class="no-list-style">
+                                <li>End of Lease Cleaning</li>
+                                <li>Aribnb Cleaning</li>
+                                <li>Carpet Steam Cleaning</li>
+                                <li>Home Cleaning</li>
+                                <li>One-off Cleaning</li>
+                                <li>Office Cleaning</li>
+                            </ul>
                         </div>
-                        <a href="https://goo.gl/maps/kLFiCLmq2e22">
-                            <i><img src="{{asset('images/newfrontend/iconmap.png')}}" pagespeed_url_hash="3185554433" onload=""/></i>
-                            <p>G4/566 St Kilda Rd, Melbourne VIC 3004</p>
-
-
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <i><img src="{{asset('images/newfrontend/icontell2.png')}}" pagespeed_url_hash="2489735862" onload=""/></i>
-                            <div class="newphone">
-                                <span>Phone Number: </span>
-                                <div class="ftell">
-                                    <p> 03 9563 2204 (9:00 - 17:00) </p>
-                                    <p>0433 588 517  (After hour) </p>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <i><img src="{{asset('images/newfrontend/iconevo.png')}}" pagespeed_url_hash="2828488535" onload=""/></i>
-                            <p>Email: info@newboda.com</p>
-                        </a>
-                    </li>
-                </ul>
+                        <div class="col-lg-4">
+                            <ul class="no-list-style">
+                                <li>Warehouse Cleaning</li>
+                                <li>Factory Cleaning</li>
+                                <li>Weeding</li>
+                                <li>Gardening</li>
+                                <li>Wall Patch</li>
+                                <li>Wall Painting</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul class="no-list-style">
+                                <li>Leak Fixing</li>
+                                <li>Floor Polishing</li>
+                                <li>Rubbish Removal</li>
+                                <li>Toilet Unblock</li>
+                                <li>Fence Repair</li>
+                                <li>Maintenance</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="footer-widget">
+                    <h3>SERVED AREA</h3>
+                    <div class="row">
+                        <div class="col-6">
+                            <ul class="no-list-style">
+                                <li>Melbourne CBD</li>
+                                <li>Box Hill</li>
+                                <li>Bundoora</li>
+                                <li>Caulfield</li>
+                                <li>Clayton</li>
+                                <li>Doncaster</li>
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <ul class="no-list-style">
+                                <li>Glen Waverley</li>
+                                <li>Heidelberg</li>
+                                <li>Richmond</li>
+                                <li>South Yarra</li>
+                                <li>South Melbourne</li>
+                                <li><strong>More Area</strong></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-    <div class="downbgblue">
-        <div class="row copy">
-            <span>Copyright @ <a href="http://nbd.com.au">www.nbd.com.au</a></span>
-            <span>nbd cleaning service</span>
-            <span><a href="/related-articles">articles</a>
-        </div>
-        <div class="row design">
-            <span>Designed by <a href="http://legenddigital.com.au/">Legend Webdesign</a></span>
-        </div>
-    </div>
-</div>
+</footer>
 @include(_get_frontend_layout_path('frontend.schema'))
 <!-- mobile footer -->
 <div class="mobfooter">
--- resources/views/frontend/custom/newboda/layouts/frontend/head.blade.php
+++ resources/views/frontend/custom/newboda/layouts/frontend/head.blade.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="{{asset('images/newfrontend/favicon.ico')}}"/>
     <link rel="bookmark" href="{{asset('images/newfrontend/favicon.ico')}}" type="image/x-icon" 　 />
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/font-awesome.min.css')}}"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" type="text/css" href="{{ url('/css/css.css')}}"/>
     <link rel="stylesheet" type="text/css" href="{{ url('/css/article.css')}}"/>
     <link rel="stylesheet" type="text/css" href="{{ url('/css/datepicker.css')}}"/>
