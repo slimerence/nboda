4c3ad2403dfd371c4c67b211a791d0e77a4fe066
slimerence <39113678+slimerence@users.noreply.github.com>
2018-08-07 17:55:23 +1000

    首页更新

--- resources/views/frontend/custom/newboda/pages/elements/home.blade.php
+++ resources/views/frontend/custom/newboda/pages/elements/home.blade.php
@@ -1,21 +1,74 @@
-<div class="conbanner indexban">
-    <div class="absobanimg">
-        <img src="{{asset('images/newfrontend/bg02.png')}}" pagespeed_url_hash="2486850179" onload="">
-    </div>
-    <div class="bgconimg">
-        <img src="{{asset('images/newfrontend/bg01.png')}}" pagespeed_url_hash="2192350258" onload="">
-    </div>
-    <div class="lefttriangle"></div>
-    <div class="wrap clearfix">
-        @include(_get_frontend_layout_path('frontend.navbox'))
-        <div class="bantxt">
-            <!--手机版 banner 新增 logo -->
-            <div class="mobbanlogo">
-                <img src="{{asset('images/newfrontend/moblogo.png')}}" pagespeed_url_hash="1334237209" onload="">
+<section id="main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2">
+                @include(_get_frontend_layout_path('frontend.navbox'))
             </div>
-            <!-- end -->
-            <img src="{{asset('images/newfrontend/bg04.png')}}" pagespeed_url_hash="3075850021" onload="">
-            <a href="/getrates">GET RATES</a>
+            <div class="col-lg-8">
+            <h2>Choose us, choose to be looked after - </h2>
+            <img src="{{asset('images/newfrontend/slo.png')}}" alt="slo" class="img-fluid" style="margin-bottom: 40px;">
+                <div class="row">
+                    <div class="col">
+                        <div class="main-swrap">
+                            <img src="{{asset('images/newfrontend/ser01.png')}}" alt="ser01">
+                            <h3>END OF LEASE CLEAN</h3>
+                            <h4>FROM <span class="color-normal">$150</span></h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="main-swrap">
+                            <img src="{{asset('images/newfrontend/ser03.png')}}" alt="ser01">
+                            <h3>AIRBNB CLEAN</h3>
+                            <h4>FROM <span class="color-normal">$60</span></h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="main-swrap">
+                            <img src="{{asset('images/newfrontend/ser09.png')}}" alt="ser01">
+                            <h3>GENERAL CLEAN</h3>
+                            <h4>FROM <span class="color-normal">$40</span>/h</h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="main-swrap">
+                            <img src="{{asset('images/newfrontend/ser10.png')}}" alt="ser01">
+                            <h3>COMMERCIAL CLEAN</h3>
+                            <h4>FREE QUOTE ON SITE</h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="main-swrap">
+                            <img src="{{asset('images/newfrontend/ser05.png')}}" alt="ser01">
+                            <h3>CARPET STEAM CLEAN</h3>
+                            <h4>FROM <span class="color-normal">$70</span></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 text-center">
+                        <div class="btn-wrap">
+                            <a href="{{ url('/services') }}" class="bg-btn">MORE SERVICES</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 text-center">
+                        <div class="btn-wrap">
+                            <a href="{{ url('/getrates') }}" class="bg-btn">GET A FREE QUOTE</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-inline-block">
+                            <img class="img-fluid" src="{{asset('images/newfrontend/partner.png')}}" alt="partner">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="offset-lg-2">
+                <div class="abs-img">
+                    <img src="{{asset('images/newfrontend/bg02.png')}}" alt="slo">
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</section>
--- resources/views/frontend/custom/newboda/pages/home.blade.php
+++ resources/views/frontend/custom/newboda/pages/home.blade.php
@@ -1,211 +1,72 @@
 @extends(_get_frontend_layout_path('frontend'))
 @section('title','Commercial Cleaning Services Melbourne | House Cleaning Melbourne ')
+
 @section('seoconfig')
     <meta name="keywords" content="Melbourne Regular Cleaning | house cleaning services | commercial cleaning">
     <meta name="description" content="NBD is a professional Commercial Cleaning expert provides thorough Melbourne Regular Cleaning services for both Commercial Cleaning and House Cleaning Services intended to keep your office and house clean and in tip top condition."/>
- @endsection
+@endsection
+
 @section('content')
     <!-- index -->
 
 
 
-    <div class="indexaboutus clearfix">
-        <div class="row leftarticle">
-            <div class="bgarimg"></div>
-            <div class="cssTable">
-                <div class="cssTd">
-                    <article>
-                        <h1>Commercial Cleaning Services Melbourne</h1>
-                        <div class="block">
-                            <h2 style="font-weight: bold; margin: 1em 0; font-size: 16px;">Australian Owned Commercial Cleaning Services and House Cleaning Expert In Melbourne | Newboda</h2>
-                            <p>Newboda is an Australian owned company that you can trust for all of your commercial cleaning services and House Cleaning needs in Melbourne. As a professional Commercial Cleaning and House Cleaning Service Provider, Newboda is committed to achieving excellence within the cleaning service industry because Newboda believes a safe and clean working environment is the basic condition that every company or family deserves.</p>
-                            <h2 style="font-weight: bold; margin: 1em 0; font-size: 16px;">Your Best Choice of Commercial Cleaning Services and House Cleaning Expert In Melbourne | Newboda</h2>
-                            <p>NBD provides Commercial Cleaning Services and House Cleaning Services intended to keep your office and your house clean and in tip-top condition.<br>
-                                NBD is not just satisfied with doing simple cleaning service, NBD aims to build an authentic and rewarding relationship with you, ensuring that you consider us a part of their team, will have a long-lasting cooperation with us.<br/>
-                                NBD cleaners are well trained to detect areas of soiling that others miss. They will see that no area of your houses or offices is overlooked and no spot survives. Covering the entire spectrum of commercial cleaning and house cleaning needs, from cleaning each strand of your places, to cleaning each pore of your tiles, we take pride in our work. Our expert staff will leave your contents with a showroom appearance once they have completed the job. Our philosophy is “ Choose us, Choose to be looked after”.
-                            </p>
-                        </div>
-                        <a href="/contactus" class="viewmore">
-                            <i class="fa fa-angle-down"></i>
-                            <p>VIEW MORE</p>
-                        </a>
-                    </article>
+    <section id="why" class="bg-white" style="padding: 80px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 my-auto text-center">
+                    <img src="{{asset('images/newfrontend/why.png')}}" alt="why" style="max-width: 150px;" class="img-fluid">
                 </div>
-            </div>
-        </div>
-        <div class="row rightbtnbox">
-            <div class="cssTable">
-                <div class="cssTd">
-                    <a href="/contactus" class="">
-                        <div class="box">
-                            <i class="fa fa-phone"></i>CONTACT US
-                        </div>
-                    </a>
-                    <a href="/getrates" class="">
-                        <div class="box">
-                            <i><img src="{{asset('images/newfrontend/iconbook.png')}}" pagespeed_url_hash="340201434" onload=""></i>BOOK NOW
-                        </div>
-                    </a>
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center my-auto">
+                    <div class="why-wrap">
+                        <img src="{{asset('images/newfrontend/w1.png')}}" alt="why">
+                        <h3>50000+</h3>
+                        <h4>SERVED CUSTOMERS</h4>
+                        <p>We have served more than 50,000 customers in the past 8 years within the Melbourne metropolitan area.</p>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="righttriangle"> </div>
-    </div>
-
-    <div class="indexservice">
-        <div class="wrap clearfix">
-            <div class="contopTit">
-                <h3 style="font-size: 25px;margin: 0 0 10px 0;color: #000;font-weight: bold;">OUR SERVICES</h3>
-                <p>We provide the safest and best cleaning service in Melbourne</p>
-            </div>
-            <div class="serlistbox">
-                <ul class="clearfix">
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser01.png')}}" pagespeed_url_hash="1611111099" onload="">
-                        </div>
-                        <p>End of Lease Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser02.png')}}" pagespeed_url_hash="1905611020" onload="">
-                        </div>
-                        <p>High Pressure Water Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser03.png')}}" pagespeed_url_hash="2200110941" onload="">
-                        </div>
-                        <p>One-off Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser04.png')}}" pagespeed_url_hash="2494610862" onload="">
-                        </div>
-                        <p>Regular Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser05.png')}}" pagespeed_url_hash="2789110783" onload="">
-                        </div>
-                        <p>Carpet Steam Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser06.png')}}" pagespeed_url_hash="3083610704" onload="">
-                        </div>
-                        <p>Specialist Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser07.png')}}" pagespeed_url_hash="3378110625" onload="">
-                        </div>
-                        <p>Pre and Post Pest Control Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser08.png')}}" pagespeed_url_hash="3672610546" onload="">
-                        </div>
-                        <p>After Build Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser09.png')}}" pagespeed_url_hash="3967110467" onload="">
-                        </div>
-                        <p>Office Clean</p>
-                    </li>
-                    <li>
-                        <div class="topimg">
-                            <img src="{{asset('images/newfrontend/ser10.png')}}" pagespeed_url_hash="1241395165" onload="">
-                        </div>
-                        <p>Urgent Clean</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <div class="indexcontact clearfix">
-        <div class="row leftimg">
-            <img src="{{asset('images/newfrontend/bg08.jpg')}}" pagespeed_url_hash="4253747001" onload="">
-        </div>
-        <div class="row rightarticle">
-            <div class="cssTable">
-                <div class="cssTd">
-                    <div class="logoimg">
-                        <img src="{{asset('images/newfrontend/logo.png')}}" pagespeed_url_hash="469326011" onload=""/>
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center my-auto">
+                    <div class="why-wrap">
+                        <img src="{{asset('images/newfrontend/w2.png')}}" alt="why">
+                        <h3>60+</h3>
+                        <h4>CLEANING EXPERTS</h4>
+                        <p>All our clenaing experts are holding a valid National Police Check and were strictly and thoroughly trained.</p>
                     </div>
-                    <div class="article">
-                        <p>CHOOSE US,</br>CHOOSE TO BE LOOKED AFTER</p>
-                    </div>
-                    <div class="btncontact">
-                        <a href="/contactus">CONTACT US</a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center my-auto">
+                    <div class="why-wrap">
+                        <img src="{{asset('images/newfrontend/w3.png')}}" alt="why">
+                        <h3>100%</h3>
+                        <h4>BOND RECOVERY</h4>
+                        <p>We cooperate with many leading real estate agency and thus we offer you a 100% bond recovery guarantee.</p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
-    <div class="indexwhy">
-        <div class="wrap">
-            <div class="choosebox">
-                <div class="contopTit">
-                    <h3 style="font-size: 25px;margin: 0 0 10px 0;color: #000;font-weight: bold;">WHY CHOOSE US?</h3>
-                    <p>We provide the safest and best cleaning service in Melbourne</p>
-                </div>
-                <div class="whylistbox">
-                    <ul class="clearfix">
+
+    <section id="comments" class="bg-light">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="offset-lg-2 col-lg-6 my-auto col-md-12">
+                    <ul class="no-list-style">
                         <li>
-                            <div class="topimg">
-                                <i></i>
-                            </div>
-                            <p>All cleaners have public  </br> liability insurance </p>
+                            <h4>Jimmy CARSON</h4>
+                            <p>"I searched on Google and find NBD Cleaning. It was not listed on the top of the page, but I made the right choice. All marks on the wall were washed off, the bathroom, even the gap between tiles were thoroughly cleaned. And thanks for that, I had my bond fully refunded."</p>
                         </li>
                         <li>
-                            <div class="topimg">
-                                <i></i>
-                            </div>
-                            <p>All cleaners have clean </br> police record</p>
-                        </li>
-                        <li>
-                            <div class="topimg">
-                                <i></i>
-                            </div>
-                            <p>Customer reviews used </br> to continously improve quality</p>
+                            <h4>Rachael LIM</h4>
+                            <p>"I find NBD Cleaning because I want a reliable cleaner to clean my Airbnb partment. My Airbnb located in Box Hill, the cleaners came on time and finished the job quickly yet perfectly. Everything is organised and I’ll definitely recommend NBD Cleaning to all Airbnb hosts."</p>
                         </li>
                     </ul>
                 </div>
-                <!--手机版新增 get rates-->
-                <div class="mobgetrates">
-                    <a href="/getrates">GET RATES</a>
+                <div class="col-lg-4 reblock">
+                    <div class="comment-img">
+                        <img src="{{asset('images/newfrontend/bg09.png')}}" alt="testimonial" class="w-100">
+                    </div>
                 </div>
-                <!-- end -->
             </div>
         </div>
-    </div>
-
-    <div class="testimonials clearfix">
-        <div class="row leftmain">
-            <div class="topslogan">TESTIMONIALS</div>
-            <div class="block">
-                <ul>
-
-                    <li>
-                        <p>&ldquo; I could not be more happier Jason was polite understanding fast working happy to explain what he did very affordable highly recommended. &rdquo;
-                        </p>
-                        <span>- Harry Bo</span>
-                    </li>
-                    <li>
-                        <p>&ldquo; Thank you for your cleaning job, peter and his team came on Thursday Morning, and immediately set to work knowing what need to be done, after four hours the house was as brand new, the agent refund the full amount of bonds to me after inspecting the house. I would definitely recommend them and would get back to them if I have something else need to be done&rdquo;
-                        </p>
-                        <span>- John Meyers </span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="row rightimg">
-            <img src="{{asset('images/newfrontend/bg09.png')}}" pagespeed_url_hash="253382330" onload="">
-        </div>
-    </div>
-
+    </section>
 @stop
